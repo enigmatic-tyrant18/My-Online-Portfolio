83cd84f666a489ab071fad12262fc14e882bf2ef
AYUSH BHAGAT <96322425+enigmatic-tyrant18@users.noreply.github.com>
2023-09-08 13:48:42 +0530

    .

--- index.js/app.js
+++ index.js/app.js
@@ -9,3 +9,18 @@
     times.addEventListener('click', function(){
         mobileNav.classList.remove('open');  
     });
+
+    var contactDetails = {
+      name: "Ayush Bhagat",
+      email: "bhagatayush710@gmail.com",
+      phone: "+91 9536977782"
+    };
+    
+
+    function contact() {
+      // alert("Your contact details are:");
+      alert("Name: " + contactDetails.name);
+      alert("Email: " + contactDetails.email);
+      alert("Phone: " + contactDetails.phone);
+    }
+    
